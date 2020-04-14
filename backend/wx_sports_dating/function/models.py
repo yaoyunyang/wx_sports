a6fb91fed9df60ae2ff321a77095782661caaa5c
Nineteen_N_E <m18827608163@163.com>
2020-04-14 15:37:44 +0800

    re_init back

---
+++ backend/wx_sports_dating/function/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Account(models.Model):
+    id_account = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50)
+    gender = models.IntegerField(default=0)
+    age = models.IntegerField(default=0)
+    profile = models.CharField(max_length=255, blank=True)
+    state = models.IntegerField(default=0)
+
+
+class Invitation(models.Model):
+    id_invitation = models.AutoField(primary_key=True)
+    inviter_id_account = models.ForeignKey('Account', on_delete=models.CASCADE)
+    gym_id_gum = models.ForeignKey('Gym', on_delete=models.CASCADE)
+    sports_type = models.IntegerField()
+    deadline = models.DateTimeField()
+    begin_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    max_responsed = models.IntegerField()
+    brif_introduction = models.CharField(max_length=255)
+    state = models.IntegerField()
+    inviter_state = models.IntegerField()
+
+
+class Gym(models.Model):
+    id_gym = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    heat = models.IntegerField(default=100)
+    time = models.CharField(max_length=50, blank=True)
+    charge = models.CharField(max_length=50, blank=True)
+    peak_time = models.CharField(max_length=50, default='16:00-18:00')
+
+
+class Message(models.Model):
+    id_message = models.AutoField(primary_key=True)
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField()
+    type = models.IntegerField()
+    state = models.IntegerField()
+    receiver_id = models.CharField(max_length=50)
+    sender_id = models.CharField(max_length=50)
+
+
+class GymComment(models.Model):
+    id_gym_comment = models.AutoField(primary_key=True)
+    account_id_account = models.ForeignKey('Account', on_delete=models.CASCADE)
+    gym_id_gym = models.ForeignKey('Gym', on_delete=models.CASCADE)
+    comment = models.CharField(max_length=255)
+
+
+class Follow(models.Model):
+    id_follow = models.AutoField(primary_key=True)
+    follower = models.ForeignKey('Account', related_name='follower_account', on_delete=models.CASCADE)
+    followed = models.ForeignKey('Account',  related_name='followed_account',on_delete=models.CASCADE)
+    invite_num = models.IntegerField()
+
+
+class Responder(models.Model):
+    id_responder = models.AutoField(primary_key=True)
+    invitation_id_invitation = models.ForeignKey('Invitation', on_delete=models.CASCADE)
+    account_id_account = models.ForeignKey('Account', on_delete=models.CASCADE)
+    state = models.IntegerField()
---
+++ backend/wx_sports_dating/wx_sports_dating/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wx_sports_dating project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wx_sports_dating.settings')
+
+application = get_asgi_application()
