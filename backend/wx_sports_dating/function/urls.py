2cdaef47ce548f054ce208dd3ee3a0b208a9a0a3
Nineteen_N_E <m18827608163@163.com>
2020-04-20 01:19:44 +0800

    login function 1.0

--- backend/wx_sports_dating/function/urls.py
+++ backend/wx_sports_dating/function/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('my_follows', views.get_my_follows),
     path('evaluate_gym', views.evaluate_gym),
     path('send_message', views.send_message),
-    path('gym_is_exist', views.gym_is_exist)
+    path('gym_is_exist', views.gym_is_exist),
+    path('login', views.login)
 ]
