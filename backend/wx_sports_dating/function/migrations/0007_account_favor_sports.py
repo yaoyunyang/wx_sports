92518b936b50f15812895a938d338b78060775c4
Nineteen_N_E <m18827608163@163.com>
2020-04-22 02:54:04 +0800

    change the num of migrations

--- backend/wx_sports_dating/function/migrations/0007_account_favor_sports.py
+++ backend/wx_sports_dating/function/migrations/0007_account_favor_sports.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('function', '0005_login'),
+        ('function', '0006_gym_brief_introduction'),
     ]
 
     operations = [
