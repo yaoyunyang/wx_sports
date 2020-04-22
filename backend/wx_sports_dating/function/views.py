0118406bfd3eb7eb2db5ae4e1200d5738f589b46
Nineteen_N_E <m18827608163@163.com>
2020-04-22 20:40:11 +0800

    get respond list function 2.0 delete some charfield

--- backend/wx_sports_dating/function/views.py
+++ backend/wx_sports_dating/function/views.py
@@ -234,14 +234,15 @@ def respond_list(request):
         invitations = models.Invitation.objects.filter(gym_id_gym_id=p_key)
         for invitation in invitations:
             invitation_dic = {}
+            invitation_dic['id'] = invitation.id_invitation
             invitation_dic['sports_type'] = invitation.sports_type
-            invitation_dic['deadline'] = invitation.deadline
+            # invitation_dic['deadline'] = invitation.deadline
             invitation_dic['begin_time'] = invitation.begin_time
-            invitation_dic['end_time'] = invitation.end_time
-            invitation_dic['brif_introduction'] = invitation.brif_introduction
+            # invitation_dic['end_time'] = invitation.end_time
+            # invitation_dic['brif_introduction'] = invitation.brif_introduction
             invitation_dic['max_responsed'] = invitation.max_responsed
-            invitation_dic['state'] = invitation.state
-            invitation_dic['inviter_state'] = invitation.inviter_state
+            # invitation_dic['state'] = invitation.state
+            # invitation_dic['inviter_state'] = invitation.inviter_state
             inviter_name = models.Account.objects.filter(open_id=invitation.inviter_open_id).get().name
             invitation_dic['inviter_name'] = inviter_name
             invitations_list.append(invitation_dic)
