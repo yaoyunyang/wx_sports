b8235c049d8be88d76dec9e3ec9036cfdfbc9de8
Nineteen_N_E <m18827608163@163.com>
2020-06-10 20:48:15 +0800

    chun

--- backend/wx_sports_dating/function/views1.py
+++ backend/wx_sports_dating/function/views1.py
@@ -25,14 +25,25 @@ def get_notice(request):
         open_id = models.Login.objects.filter(hash_id=hash_id).last().open_id
         account = models.Account.objects.filter(open_id=open_id).first()
         my_invitation = models.Invitation.objects.filter(inviter_open_id=open_id)
-        my_respond = models.Invitation.objects.filter(invitation__account_id_account=account)
+        my_respond_list = models.Invitation.objects.filter(invitation__account_id_account=account)
+        message = models.Message.objects.filter(receiver_id=account.id_account)
+        my_respond = []
         message = models.Message.objects.filter(receiver_id=account.id_account)
         for invitation in my_invitation:
             invitation.inviter_state = len(invitation.invitation.all())
-        for invitation in my_respond:
-            invitation.inviter_state = len(invitation.invitation.all())
+        for invitation in my_respond_list:
+            respond = {}
+            temp_account = models.Account.objects.filter(open_id=invitation.inviter_open_id).first()
+            respond['inviter_state'] = len(invitation.invitation.all())
+            respond['max_responsed'] = invitation.max_responsed
+            respond['invitation_id'] = invitation.id_invitation
+            respond['inviter_name'] = temp_account.name
+            respond['inviter_avatar'] = temp_account.avatar
+            respond['sports_type'] = invitation.sports_type
+            respond['deadline'] = invitation.deadline
+            my_respond.append(respond)
         response['my_inv'] = json.loads(serializers.serialize("json", my_invitation))
-        response['my_respond'] = json.loads(serializers.serialize("json", my_respond))
+        response['my_respond'] = my_respond
         response['message'] = json.loads(serializers.serialize("json", message))
         response['status'] = 200
     except Exception as exception:
